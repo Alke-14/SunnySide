312729b10c4a5770f5a8f36eac2214325958f757
Kevin A Hernandez Garcia <kevinalbertohernandezgarcia@gmail.com>
2025-11-12 13:33:04 -0600

    feat: updated format of app to pick between City input and AI Assistant chat (needs AI implementation)

--- backend/backend.py
+++ backend/backend.py
@@ -6,18 +6,24 @@ from fastapi.middleware.cors import CORSMiddleware
 import requests
 import os
 
+# declare the FastAPI function
 app = FastAPI()
 
+# set the path to the static files (React build output)
 static_dir = os.path.join(os.path.dirname(__file__), '../dist')
 
+# setup CORS to process requrests from frontend
 app.add_middleware(
     CORSMiddleware,
+    # these are the origins used in development and production, it escapes CORS error handler;
+    # otherwise, any requests we do through the field will be blocked and logged in the console from the browser (I found out the hard way :( )
     allow_origins=["http://localhost:5173", "https://sunnyside-91z4.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+# mount the static files directory to serve React app in the assets directory (used to load essential files)
 app.mount("/assets", StaticFiles(directory=os.path.join(static_dir, "assets")), name="assets")
 
 API_KEY = "2310e340b45dfe6b4d8745df476e365f"
@@ -29,14 +35,18 @@ def get_weather(city: str):
     response = requests.get(complete_url)
     data = response.json()
 
+# if the data is found for the city, extract the specified information
     if data["cod"] != "404":
         main_data = data["main"]
+        # extracting weather description and initialize the API response
         weather_data = data["weather"][0]
+        # using the data from the response above, we extract the important information
         temperature = main_data["temp"]
         pressure = main_data["pressure"]
         humidity = main_data["humidity"]
         weather_description = weather_data["description"]
-
+        
+        # make it pretty for frontend to load properly on the container in the card  
         weather_info = (
             f"Temperature: {temperature}°C\n"
             f"Humidity: {humidity}%\n"
@@ -51,6 +61,7 @@ def get_weather(city: str):
 
 @app.get("/{full_path:path}")
 async def serve_react(full_path: str):
+    # we tell the backend to load the contents into the index for anything that is not declared as an API route
     index_path = os.path.join(static_dir, "index.html")
     if os.path.exists(index_path):
         return FileResponse(index_path)
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import { useState } from "react";
 import axios from "axios";
-
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupInput,
+  InputGroupText,
+  InputGroupTextarea,
+} from "@/components/ui/input-group"
+import { IconPlus } from "@tabler/icons-react"
+import { ArrowUpIcon, AppWindowIcon, CodeIcon } from "lucide-react"
+import { Separator } from "@/components/ui/separator"
 import {
   Card,
   CardContent,
@@ -10,16 +20,21 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Button } from "@/components/ui/button"
 
 
 function App() {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState("");
+  const [assistantResponse, setAssistantResponse] = useState("");
+  const [aiError, setAiError] = useState("");
   const [error, setError] = useState("");
 
   const getWeather = async () => {
     try {
+      // axios handles the GET request to send to the FastAPI backend using the deployed URL from Render
       const response = await axios.get(`https://sunnyside-91z4.onrender.com/weather?city=${city}`);
       setWeather(response.data.weather);
       setError("");
@@ -30,35 +45,70 @@ function App() {
   };
 
   return (
-    <div className="bg-[#E6D5AD]">
-      <div className="flex items-center justify-center h-screen p-4">
-        <Card className="p-5 h-fit">
-          <CardHeader>
-            <CardTitle>SunnySide</CardTitle>
-            <CardDescription>This is a simple weather app which retrieves data from an API called <b>OpenWeather</b> along with the use of <b>FastAPI</b> to easily connect the Python backend with the frontend</CardDescription>
-            
-          </CardHeader>
-          <CardContent>
-            <Input
-              type="text"
-              placeholder="Enter City"
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-              className="mb-2"
-            />
-            <Button onClick={getWeather}>Get Weather</Button>
+    <div className="bg-[#E6D5AD] flex justify-center">
+      <Tabs defaultValue="city" className="h-screen w-[500px] justify-center ">
+        <TabsList>
+          <TabsTrigger value="city">Find a City</TabsTrigger>
+          <TabsTrigger value="assistant">Ask AI Assistant</TabsTrigger>
+        </TabsList>
+        <TabsContent value="city">
+          <Card className="p-5 h-fit">
+            <CardHeader>
+              <CardTitle>SunnySide</CardTitle>
+              <CardDescription>This is a simple weather app which retrieves data from an API called <b>OpenWeather</b> along with the use of <b>FastAPI</b> to easily connect the Python backend with the frontend</CardDescription>
+              
+            </CardHeader>
+            <CardContent>
+              <Input
+                type="text"
+                placeholder="Enter City"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+                className="mb-2"
+              />
+              <Button onClick={getWeather}>Get Weather</Button>
 
-            <div className="border-4 rounded-sm p-2.5" style={{ marginTop: "20px", whiteSpace: "pre-wrap" }}>
-              {weather && <p>{weather}</p>}
-              {error && <p style={{ color: "red" }}>{error}</p>}
-            </div>
-          </CardContent>
-          <CardFooter>
-            &copy; 2025 Tutor Me Inc. All rights reserved.
-          </CardFooter>
-        </Card>
-        
-      </div>
+              <div className="border-4 rounded-sm p-2.5" style={{ marginTop: "20px", whiteSpace: "pre-wrap" }}>
+                {weather && <p>{weather}</p>}
+                {error && <p style={{ color: "red" }}>{error}</p>}
+              </div>
+            </CardContent>
+            <CardFooter>
+              &copy; 2025 Tutor Me Inc. All rights reserved.
+            </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="assistant">
+          <Card className="p-5">
+            <InputGroup>
+              <InputGroupTextarea className="" placeholder="Ask, Search or Chat..."/>
+              <InputGroupAddon align="block-end">
+                <InputGroupButton
+                  variant="outline"
+                  className="rounded-full"
+                  size="icon-xs"
+                >
+                  <IconPlus />
+                </InputGroupButton>
+                
+                <InputGroupText className="ml-auto"></InputGroupText>
+                <Separator orientation="vertical" className="!h-4" />
+                <InputGroupButton
+                  variant="default"
+                  className="rounded-full"
+                  size="icon-xs"
+                  disabled
+                >
+                  <ArrowUpIcon />
+                  <span className="sr-only">Send</span>
+                </InputGroupButton>
+              </InputGroupAddon>
+            </InputGroup>
+          </Card>  
+        </TabsContent>
+      </Tabs>
+      
+          
     </div>
   );
 }
